013aef78476a8d1a90d9fe216a3ba184c603b523
Srinivas <kolipakasrinivas456@gmail.com>
2022-10-09 07:58:02 +0530

    thred Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Navbar(){
+ export default function Navbar(){
     return(
         <div>
+            <h1>Hello</h1>
 
         </div>
     )
 }
 
-exports default Navbar
