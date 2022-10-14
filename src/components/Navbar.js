51d319609a96d10c1668a9b807feb132103a43a2
Srinivas <kolipakasrinivas456@gmail.com>
2022-10-14 13:30:45 +0530

    four commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../App.css';
 
 
 
  export default function Navbar(){
     return(
         <div>
-            <nav>
+            <nav className='navbar'>
                 <img src="react-icon-small.png" alt='logo'/>
                 <h2>ReactFacts</h2>
-                <h2>React Course-Project 1</h2>
+                <h3 id='cource'>React Course-Project 1</h3>
             </nav>
         </div>
     )
