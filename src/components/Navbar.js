c8ba6ae41986782e36955cf50d1fc5689a2d62b0
Srinivas <kolipakasrinivas456@gmail.com>
2022-10-14 12:21:14 +0530

    2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
+
+
  export default function Navbar(){
     return(
         <div>
-            <h1>Hello</h1>
+            <nav>
+                <img src="react-icon-small.png" alt='logo'/>
+                <h2>ReactFacts</h2>
+                <h2>React Course-Project 1</h2>
 
+            </nav>
         </div>
     )
 }
