7818fc46db9e815e3acd55b45df677ca5a98c038
Srinivas <kolipakasrinivas456@gmail.com>
2022-10-14 12:39:28 +0530

    thrid commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,13 @@ import React from 'react';
 function Main(){
     return(
         <main>
-            <h1></h1>
+            <h1>Fun facts about React</h1>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
         </main>
     )
